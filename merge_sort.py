af055bef2414d9e35ca13fb5dfbe999ea4457a96
Nujabes <nujabes@Nujabesui-MacBookAir.local>
2026-01-08 15:53:59 +0900

    2: merge sort, selection sort, insertion sort

--- merge_sort.py
+++ merge_sort.py
@@ -11,3 +11,48 @@ def bubble_sort(nums):
                 swapping=True
         end -= 1
     return nums
+
+def selection_sort(nums: list) -> None:
+    for i in range(len(nums)):
+        smallest = i
+        for j in range(i+1, len(nums)):
+            if nums[smallest]>nums[j]:
+                smallest = j
+        nums[smallest], nums[i] = nums[i], nums[smallest]
+
+def insertion_sort(nums: list) -> None:
+    for i in range(1, len(nums)):
+        for j in range(i,0,-1):
+            if nums[j-1] > nums[j]:
+                nums[j-1], nums[j] = nums[j], nums[j-1]
+            else:
+                break
+    # bit slower than selection_sort
+    # but if list is almost sorted, insertion sort is effective
+    # time complexity : O(N**2)
+
+# merge sort
+def merge_sort(L: list) -> None:
+    if len(L) <2:
+        return L
+    mid = len(L)//2
+    left= L[:mid]
+    right = L[mid:]
+    sorted_left = merge_sort(left)
+    sorted_right = merge_sort(right)
+    return merge(sorted_left, sorted_right)
+
+def merge(left: list, right: list) -> list:
+    merged = []
+    l = 0
+    r = 0
+    while l < len(left) and r < len(right):
+        if left[l] <= right[r]:
+            merged.append(left[l])
+            l += 1
+        else:
+            merged.append(right[r])
+            r += 1
+    merged.extend(left[l:])
+    merged.extend(right[r:])
+    return merged
